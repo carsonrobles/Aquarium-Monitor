a10e8a3675d1236920217f4dbfa949755eaebee5
Carson Robles <carson.robles@gmail.com>
2017-01-02 15:58:34 -0800

    added packet send module

--- rtl/SPI/src/pkt_snd.v
+++ rtl/SPI/src/pkt_snd.v
@@ -7,21 +7,28 @@
  * to send 1 packet (16 bits)
  */
 
+// fsm states
+`define ST_IDL 2'b00                                    // idle state
+`define ST_END 2'b01                                    // finished state
+`define ST_ADR 2'b10                                    // send address state
+`define ST_DAT 2'b11                                    // send content state
+
 module pkt_snd (
-    input  wire       clk,                          // system clock
+    input  wire        clk,                             // system clock
+    input  wire        preq,                            // packet send request
 
-    input  wire [7:0] addr,                         // address portion of packet
-    input  wire [7:0] dat,                          // data portion of packet
+    input  wire [15:0] pkt,                             // 16 bit packet to be sent
 
-    output wire       sclk,                         // serial clock
-    output wire       ss,                           // slave select
-    output wire       sdo                           // serial data out
+    output wire        sclk,                            // serial clock
+    output wire        load,                            // load latch
+    output wire        sdo                              // serial data out
 );
 
-reg       req = 1'b0;                               // request data send
-reg [7:0] snd = 8'h0;                               // byte to send
+reg       req = 1'b0;                                   // request data send
+reg [7:0] snd = 8'h0;                                   // byte to send
 
-wire snt;                                           // high when full byte has been sent
+wire ss;                                                // slave select (not used)
+wire snt;                                               // high when full byte has been sent
 
 // instantiate SPI module
 spi spi_sseg (
@@ -34,16 +41,66 @@ spi spi_sseg (
     .snt  (snt )
 );
 
+/* --- PKT LOGIC --- */
 
+reg [7:0] addr = 8'h0;                                  // address portion of packet
+reg [7:0] cont = 8'h0;                                  // content portion of packet
 
+always @ (posedge clk) begin
+    if (fsm == `ST_IDL) begin
+        addr <= pkt[15:8];                              // latch address portion of packet
+        cont <= pkt[ 7:0];                              // latch content portion of packet
+    end
+end
 
+always @ (posedge clk) begin
+    if (fsm_d == `ST_ADR)      snd <= addr;             // in addr state send address
+    else if (fsm_d == `ST_DAT) snd <= cont;             // in data state send content
+end
 
-wire snt_e = (sft == 2'b01);
-reg [1:0] sft = 2'b0;
+/* --- PKT LOGIC --- */
 
-always @ (posedge clk) sft <= (sft << 1) | snt;
+/* --- REQUEST LOGIC --- */
 
-always @ (posedge clk) cnt <= cnt + sft;
+always @ (posedge clk) begin
+    if (fsm == `ST_ADR || fsm == `ST_DAT) begin
+        if (req & snt)       req <= 1'b0;               // if req & snt, end current request
+        //else if (~req & snt) req <= 1'b1;               // if ~req & snt, submit new request
+        else                 req <= 1'b1;
+    end
+end
+
+/* --- REQUEST LOGIC --- */
+
+/* --- FSM --- */
+
+reg [1:0] fsm = `ST_IDL;                                // current state
+reg [1:0] fsm_d;                                        // next state
+
+// state assignment
+always @ (posedge clk) fsm <= fsm_d;
+
+// combinatorial state change logic
+always @ (*) begin
+    case (fsm)
+        `ST_IDL:
+            if (preq) fsm_d = `ST_ADR;                  // switch to send addr on request
+            else      fsm_d = `ST_IDL;
+        `ST_ADR:
+            if (req & snt) fsm_d = `ST_DAT;             // switch to send data on first done state
+            else           fsm_d = `ST_ADR;
+        `ST_DAT:
+            if (req & snt) fsm_d = `ST_END;             // finish upon final done state
+            else           fsm_d = `ST_DAT;
+        `ST_END:
+            if (~preq) fsm_d = `ST_IDL;                 // switch to idle when request is low
+            else       fsm_d = `ST_END;
+        default:
+            fsm_d = `ST_IDL;                            // send to idle in case of invalid state
+    endcase
+end
+
+/* --- FSM --- */
 
 endmodule
 
--- rtl/SPI/src/spi.v
+++ rtl/SPI/src/spi.v
@@ -9,28 +9,28 @@
  */
 
 // fsm states
-`define ST_IDL 2'b00                                // idle state
-`define ST_RUN 2'b01                                // running state
-`define ST_END 2'b10                                // finished state
+`define ST_IDL 2'b00                                    // idle state
+`define ST_END 2'b01                                    // finished state
+`define ST_RUN 2'b10                                    // running state
 
 module spi (
-    input  wire       clk,                          // system clock
-    input  wire       req,                          // request to send byte
+    input  wire       clk,                              // system clock
+    input  wire       req,                              // request to send byte
 
-    input  wire [7:0] dat,                          // byte to send
+    input  wire [7:0] dat,                              // byte to send
 
-    output reg        sclk = 1'b0,                  // serial clock
-    output reg        ss   = 1'b1,                  // slave select
-    output wire       sdo,                          // serial data out
+    output reg        sclk = 1'b0,                      // serial clock
+    output reg        ss   = 1'b1,                      // slave select
+    output wire       sdo,                              // serial data out
 
-    output wire       snt                           // high when full byte has been sent
+    output wire       snt                               // high when full byte has been sent
 );
 
 /* --- clock --- */
 
 // clock divider to half system clock
 always @ (posedge clk) begin
-    if (fsm != `ST_RUN) sclk <= 1'b0;               // serial clock only operates in RUN state
+    if (fsm != `ST_RUN) sclk <= 1'b0;                   // serial clock operates in RUN state
     else                sclk <= ~sclk;
 end
 
@@ -38,24 +38,24 @@ end
 
 /* --- data --- */
 
-reg [2:0] scnt  = 3'h0;                             // data shift count
-reg [7:0] dat_r = 8'h0;                             // data register to be shifted out
+reg [2:0] scnt  = 3'h0;                                 // data shift count
+reg [7:0] dat_r = 8'h0;                                 // data register to be shifted out
 
-assign sdo = dat_r[7];                              // sdo is MSB of dat_r (shifted left)
-assign snt = (fsm == `ST_END);                      // set high when full byte sent
+assign sdo = dat_r[7];                                  // sdo is MSB of dat_r (shifted left)
+assign snt = (fsm == `ST_END);                          // set high when full byte sent
 
 always @ (posedge clk) begin
-    if (fsm_d == `ST_RUN || fsm == `ST_RUN) ss <= 1'b0;
-    else    ss <= 1'b1;
+    if (fsm_d == `ST_RUN || fsm == `ST_RUN) ss <= 1'b0; // ss is low while byte being sent
+    else                                    ss <= 1'b1; // high during other states
 end
 
 always @ (posedge clk) begin
     if (fsm == `ST_IDL) begin
-        scnt  <= 3'h0;                              // reset scnt at idle
-        dat_r <= dat;                               // when idle, latch dat to dat_r
+        scnt  <= 3'h0;                                  // reset scnt at idle
+        dat_r <= dat;                                   // when idle, latch dat to dat_r
     end else if (sclk) begin
-        scnt  <= scnt + 3'h1;                       // increment shift count
-        dat_r <= dat_r << 1;                        // shift bit out when sclk is high (falling)
+        scnt  <= scnt + 3'h1;                           // increment shift count
+        dat_r <= dat_r << 1;                            // shift bit out on falling edge sclk
     end
 end
 
@@ -63,27 +63,26 @@ end
 
 /* --- FSM --- */
 
-reg [1:0] fsm = `ST_IDL;                            // current state
-reg [1:0] fsm_d;                                    // next state
+reg [1:0] fsm = `ST_IDL;                                // current state
+reg [1:0] fsm_d;                                        // next state
 
 // state assignment
-always @ (posedge clk)
-    if (~sclk) fsm <= fsm_d;
+always @ (posedge clk) fsm <= fsm_d;
 
 // combinatorial state change logic
 always @ (*) begin
     case (fsm)
         `ST_IDL:
-            if (req) fsm_d = `ST_RUN;               // begin running when request issued
-            else     fsm_d = `ST_IDL;               // stay in idle if no request
+            if (req) fsm_d = `ST_RUN;                   // begin running when request issued
+            else     fsm_d = `ST_IDL;                   // stay in idle if no request
         `ST_RUN:
-            if (&scnt) fsm_d = `ST_END;             // if full byte sent, finish
-            else       fsm_d = `ST_RUN;             // stay running if not yet sent
+            if (&scnt) fsm_d = `ST_END;                 // if full byte sent, finish
+            else       fsm_d = `ST_RUN;                 // stay running if not yet sent
         `ST_END:
-            if (~req) fsm_d = `ST_IDL;              // back to idle after finish
-            else      fsm_d = `ST_END;              // stay in end until request is pulled low
+            if (~req) fsm_d = `ST_IDL;                  // back to idle after finish
+            else      fsm_d = `ST_END;                  // stay in end until request is low
         default:
-            fsm_d = `ST_IDL;                        // send to idle in case of invalid state
+            fsm_d = `ST_IDL;                            // send to idle in case of invalid state
     endcase
 end
 
